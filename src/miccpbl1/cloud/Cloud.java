1975c4387124efb32766edc8b4cbd050f6c97139
MrGustavo <ghboliveira@hotmail.com>
2017-10-25 23:14:21 -0300

    Create the cloud

---
+++ src/miccpbl1/cloud/Cloud.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miccpbl1.cloud;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import miccpbl1.cloud.controller.Controller;
+import miccpbl1.model.Medico;
+import miccpbl1.model.Paciente;
+
+/**
+ *
+ * @author gustavo
+ */
+public class Cloud {
+
+    private Controller controllerServer = null;
+
+    private final int port = 12345;
+
+    private final int lengthCodeProtocol = 2;
+
+    private DatagramSocket serverSocket = null;
+    private DatagramPacket receivePacket = null;
+
+    byte[] receiveData = null;
+    byte[] sendData = null;
+
+    /**
+     * The main method.
+     * @param args 
+     */
+    public static void main(String args[]) {
+        try {
+            new Cloud().startServer();
+        } catch (SocketException ex) {
+            Logger.getLogger(Cloud.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Initialize the server, for to receiver connections and identify the actions.
+     * @throws SocketException 
+     */
+    private void startServer() throws SocketException {
+        controllerServer = Controller.getController();
+        serverSocket = new DatagramSocket(port);
+        receiveData = new byte[1024];
+
+        while (true) {
+            try {
+                receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                serverSocket.receive(receivePacket);
+
+                Runnable run;
+                run = new Runnable() {
+                    @Override
+                    public void run() {
+                        String data = new String(receivePacket.getData());
+                        identifyAction(data);
+                    }
+
+                    private void identifyAction(String data) {
+                        String initCode = data.substring(0, lengthCodeProtocol);
+                        int lastCodeIndex = data.lastIndexOf(initCode);
+                        if (lastCodeIndex == 0) {
+                            return;
+                        }
+                        String endCode = data.substring(lastCodeIndex, lastCodeIndex + lengthCodeProtocol);
+                        if (!initCode.equals(endCode)) {
+                            return;
+                        } else {
+                            data = data.substring(lengthCodeProtocol, lastCodeIndex);
+                            switch (initCode) {
+                                case "00":
+                                    System.out.println("Paciente Registrado!");
+                                    System.out.println(data);
+                                    controllerServer.registerPacient(data);
+                                    break;
+                                case "01":
+                                    System.out.println("Atualizando dados do Paciente");
+                                    System.out.println(data);
+                                    controllerServer.refreshStatusPacient(data);
+                                    break;
+                                case "08":
+                                    String stri = controllerServer.mountListPatient();
+                                    sendDatagramPacket(stri);
+                                    break;
+                                case "09":
+                                    System.out.println("Testando Conexão");
+                                    System.out.println(data);
+                                    sendDatagramPacket("0x09testSucessful0x09");
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                    }
+                };
+                new Thread(run).start();
+            } catch (IOException ex) {
+                Logger.getLogger(Cloud.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    /**
+     * Send datagrams for the client connected.
+     * @param data - Data to send.
+     */
+    private void sendDatagramPacket(String data) {
+        try {
+            DatagramPacket sendPacket;
+            sendData = data.getBytes();
+            sendPacket = new DatagramPacket(sendData, sendData.length, receivePacket.getAddress(), receivePacket.getPort());
+            serverSocket.send(sendPacket);
+        } catch (IOException ex) {
+            Logger.getLogger(Cloud.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
---
+++ src/miccpbl1/cloud/controller/Controller.java
@@ -0,0 +1,239 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miccpbl1.cloud.controller;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Iterator;
+import miccpbl1.model.Medico;
+import miccpbl1.model.Paciente;
+import miccpbl1.server.Server;
+
+/**
+ * Class of control the connections between the servers and clients. Storage too
+ * the data of patients in risk, servers and doctors.
+ *
+ * @author Gustavo
+ */
+public class Controller implements Serializable {
+
+    private final String TOKENSEPARATOR = "!=";
+    private final int LENGTHSERVERPROTOCOL = 4;
+    private final int LENGTHPROTOCOL = 2;
+
+    private static Controller controller = null;
+    private final ArrayList<Paciente> listaPacientes = new ArrayList<>();
+    private final ArrayList<Medico> listDoctor = new ArrayList<>();
+    private final ArrayList<String> listServers = new ArrayList<>();
+
+    /**
+     * Singleton getController
+     * @return controller - Current controller instance.
+     */
+    public static Controller getController() {
+        if (controller == null) {
+            controller = new Controller();
+        }
+        return controller;
+    }
+
+    /**
+     * Reset the controller
+     */
+    public static void resetController() {
+        controller = null;
+    }
+
+    /**
+     * Register the patient on cloud.
+     * @param data - Data about the patient.
+     */
+    public void registerPacient(String data) {
+        String nome;
+        String cpf;
+        String numero;
+
+        Paciente paciente;
+        String[] splitData = data.split(TOKENSEPARATOR);
+
+        nome = splitData[0];
+        cpf = splitData[1];
+        numero = splitData[2];
+        paciente = new Paciente(nome, cpf, numero);
+
+        if (findPacient(cpf) == null) {
+            System.out.println("Cadastrando Paciente");
+            listaPacientes.add(paciente);
+        } else {
+            System.out.println("Não Cadastrando Paciente");
+            return;
+        }
+
+    }
+
+    /**
+     * Register the server on cloud.
+     * @param data - Data with ip and location of server.
+     */
+    public void registerServer(String data) {
+        if (data == null) {
+            return;
+        } else if (data.trim().isEmpty()) {
+            return;
+        } else {
+            listServers.add(data);//The data will come formated.
+        }
+    }
+
+    /**
+     * Method for update the status of patient, these case, if the data is
+     * received, then the patient is in risk.
+     *
+     * @param data - cpf*btCardiaco*repouso*pressao*risco, * = string separator.
+     */
+    public void refreshStatusPacient(String data) {
+        String[] splitedData = splitString(data, TOKENSEPARATOR);
+        Paciente patient = findPacient(splitedData[0]);
+        if (patient == null) {
+            return;
+        } else {
+            System.out.println("Atualizando dados do paciente");
+            patient.getListBtCardiacos().add(splitedData[1]);
+            patient.getListRepouso().add(splitedData[2]);
+            patient.getListPressaoSanguinea().add(splitedData[3]);
+            patient.getListAcEspecial().add("Risk");
+        }
+    }
+
+    /**
+     * Search the patient registered using the cpf as parameter.
+     * @param cpf - Identifier of patient.
+     * @return patient - Case exist patient, null case not.
+     */
+    private Paciente findPacient(String cpf) {
+        Iterator<Paciente> it = listaPacientes.iterator();
+        Paciente findPacient;
+        while (it.hasNext()) {
+            findPacient = it.next();
+            if (findPacient.getCPF().equals(cpf)) {
+                return findPacient;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Method for split the string received.
+     * @param string - String.
+     * @param splitString - String separator.
+     * @return string - String divided.
+     */
+    private String[] splitString(String string, String splitString) {
+        return string.split(splitString);// Use the method java.
+    }
+
+    /**
+     * Return string with the data of patients that are on string received.
+     * @param patients - Cpf's of patients.
+     * @return string - String with the patients chosen.
+     */
+    private String getDataPatients(String[] patients) {
+        String data = new String();
+        int i = 0;
+        Iterator<Paciente> it = listaPacientes.iterator();
+        Paciente patient;
+        while (it.hasNext()) {
+            patient = it.next();
+            if (patients[i].equals(patient.getCPF())) {
+                data += patient.getCPF();
+                data += TOKENSEPARATOR;
+                data += patient.getListBtCardiacos().get(0);
+                data += TOKENSEPARATOR;
+                data += patient.getListPressaoSanguinea().get(0);
+                data += TOKENSEPARATOR;
+                data += patient.getListRepouso().get(0);
+                data += TOKENSEPARATOR;
+                data += patient.getListAcEspecial().get(0);
+                if (it.hasNext()) {
+                    data += TOKENSEPARATOR;
+                }
+            }
+        }
+        return data;
+    }
+
+    /**
+     * Search the doctor using the cpf.
+     * @param cpf - Identifier of doctor.
+     * @return doctor - Case exist, null - Case not exist.
+     */
+    private Medico findDoctor(String cpf) {
+        Iterator<Medico> it = listDoctor.iterator();
+        Medico findDoctor;
+        while (it.hasNext()) {
+            findDoctor = it.next();
+            if (findDoctor.getCPF().equals(cpf)) {
+                return findDoctor;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Mount the list with all data of patients
+     * @return string - Data of all the patients.
+     */
+    public String mountListPatient() {
+        String listPat;
+        listPat = "0x08";
+        Paciente pat;
+        Iterator<Paciente> it = listaPacientes.iterator();
+        while (it.hasNext()) {
+            pat = it.next();
+            listPat += pat.getCPF();
+            listPat += TOKENSEPARATOR;
+            listPat += pat.getNome();
+            listPat += TOKENSEPARATOR;
+            listPat += pat.getNumero();
+            if (it.hasNext()) {
+                listPat += TOKENSEPARATOR;
+            }
+        }
+        listPat += "0x08";
+        return listPat;
+    }
+    
+    /**
+     * Return the ip of server based on distance. Calculate with relation with triangle.
+     * @param x - Position in x.
+     * @param y - Position in y.
+     * @return ip - Ip of server, null - Case not exist server.
+     */
+    public String getIpServeByLocation(int x, int y){
+        String ip;
+        String[] serverData;
+        Iterator<String> it = listServers.iterator();
+        int vx, vy, dist;
+        if(listServers.isEmpty()){
+            return null;
+        }
+        serverData = listServers.get(0).split(TOKENSEPARATOR);
+        
+        vx = Integer.parseInt(serverData[0]); // Get the cordenate in x.
+        vy = Integer.parseInt(serverData[1]); // Get the cordenate in y.
+        ip = serverData[2];//It get the first ip, because the first is the more close.
+        dist = vx*vx +vy*vy;// Use baskara for calculate the distance, use the relation of triangle.
+        while(it.hasNext()){                                
+            serverData = it.next().split(TOKENSEPARATOR);
+            vx = Integer.parseInt(serverData[0]); // Get the cordenate in x.
+            vy = Integer.parseInt(serverData[1]); // Get the cordenate in y.
+            if(dist > vx*vx +vy*vy){
+                ip = serverData[2];
+            }
+        }
+        return ip;
+    }
+}
